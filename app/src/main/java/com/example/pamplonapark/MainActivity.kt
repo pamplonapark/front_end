a7c3fa67f1cb21098f817155b10867f0d2d8544a
vicsurcor <vdsurcor@gmail.com>
2024-02-07 19:01:45 +0100

    onClicks_Main

--- app/src/main/java/com/example/pamplonapark/MainActivity.kt
+++ app/src/main/java/com/example/pamplonapark/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.pamplonapark
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.room.Room
 import com.example.pamplonapark.database.DatabaseManager
+import com.example.pamplonapark.interfaces.LoginActivity
+import com.example.pamplonapark.interfaces.SignupActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var botIniciarSesion: Button
+    private lateinit var botRegistrarse: Button
+
     /* Static database init */
     companion object {
         lateinit var database: DatabaseManager
@@ -22,5 +29,19 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        botIniciarSesion = findViewById(R.id.iniciarSesion)
+        botIniciarSesion.setOnClickListener {
+
+            val cambio =  Intent(this, LoginActivity::class.java)
+            startActivity(cambio)
+        }
+
+        botRegistrarse = findViewById(R.id.registrarse)
+        botRegistrarse.setOnClickListener {
+
+            val cambio =  Intent(this, SignupActivity::class.java)
+            startActivity(cambio)
+        }
     }
 }
