760328391dd59947430fe8f213f0b53fa90b6402
Javi <jcuestarui1@educacion.navarra.es>
2024-03-12 00:04:56 +0100

    Added again navigation

--- app/src/main/java/com/example/pamplonapark/interfaces/SearchActivity.kt
+++ app/src/main/java/com/example/pamplonapark/interfaces/SearchActivity.kt
@@ -1,11 +1,13 @@
 package com.example.pamplonapark.interfaces
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.pamplonapark.MainActivity
 import com.example.pamplonapark.R
 import com.example.pamplonapark.interfaces.adapters.RowAdapter
 import com.example.pamplonapark.interfaces.adapters.items.ParkingItem
@@ -25,6 +27,7 @@ class SearchActivity : AppCompatActivity() {
 
     var isImageChanged = false
     private lateinit var btnFavoritos: ImageButton
+    private lateinit var btnReturn: ImageButton
 
     /**
      * Método llamado cuando se crea la actividad.
@@ -39,6 +42,17 @@ class SearchActivity : AppCompatActivity() {
         GlobalScope.launch(Dispatchers.Main) {
             val dataList = generateData()
             setupRecyclerView(dataList)
+        }
+        btnFavoritos = findViewById(R.id.favs)
+        btnFavoritos.setOnClickListener {
+            startActivity(Intent(this, FavoritoActivity::class.java))
+            this.finish()
+        }
+
+        btnReturn = findViewById(R.id.volver)
+        btnReturn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            this.finish()
         }
     }
 
