7b8341fbad32532eba097af560339cd4386e52bd
Randy.A <rachana@educacion.navarra.es>
2024-03-14 23:42:02 +0100

    Testing_MainActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,8 @@ dependencies {
 
     testImplementation("junit:junit:4.13.2")
     testImplementation("com.google.truth:truth:1.4.2")
+    testImplementation ("org.robolectric:robolectric:4.6.1")
+
 
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
